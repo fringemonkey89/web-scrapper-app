b82387670159a3c3462b2aad3db9eb93cdeed78f
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-10 19:39:57 -0500

    fixed bug in main.js

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const app = express();
 
 const port = 3000;
 
-app.get('/crawl',  async (req, res) =>{
+app.get('/crawl',  async (req, res, next) =>{
     const website = req.query.website;
+    
     if(!website) {
         const err = new Error("required query website missing")
         err.status = 400;
@@ -15,7 +16,7 @@ app.get('/crawl',  async (req, res) =>{
     try{
         const browser = await puppeteer.launch()
         const registry = {};
-        const queue = [!req.query.website];
+        let queue = [website];
         // const page = await browser.newPage();
 
         // await page.goto(req.query.website)
@@ -25,18 +26,29 @@ app.get('/crawl',  async (req, res) =>{
 
         while(queue.length > 0) {
             const url = queue[queue.length -1];
+            console.log("current url", url)
             const page = await browser.newPage();
             await page.goto(url);
             registry[url] = await page.$eval("*", (el) => el.innerText)
             queue.pop();
+            console.log("queue length", queue)
 
-            const href = await page.$$eval("a", (anchorEls) => anchorEls.map((a) => a.href))
+            const hrefs = await page.$$eval("a", (anchorEls) => anchorEls.map((a) => a.href))
 
-            const filteredHrefs = href.filter((href) => href.startsWith(website) && registry[href] === undefined)
-            const uniqueHrefs = [...new Set]
+            const filteredHrefs = hrefs.filter(
+                (href) => href.startsWith(website) && registry[href] === undefined
+                );
+                
+            const uniqueHrefs = [...new Set(filteredHrefs)]
+            queue.push(...uniqueHrefs)
+            queue = [...new Set[queue]]
+
+            await page.close();
         }
 
-        return res.status(200).send(html)
+        browser.close();
+
+        return res.status(200).send(registry)
         
     }catch(e) {
         console.log(e)
